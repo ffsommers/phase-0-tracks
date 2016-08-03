05898d6bf8623fb353a0279af745c7570066285b
Frank Sommers <franksommers@Franks-MacBook-Pro.local>
2016-08-03 11:05:10 -0700

    got data from user. name, age, year.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+def vampire_check 
+  puts "What is your name?"
+  name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp
+  puts "What year were you born in?"
+  bday= gets.chomp 
+  puts name
+  puts age 
+  puts bday
+
+end
+
+puts vampire_check
