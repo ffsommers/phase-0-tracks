7a4a953c508404fd65c5566619820bd6372f1145
Frank Sommers <franksommers@Franks-MacBook-Pro.local>
2016-08-03 11:48:07 -0700

    created if else blocks for checking if person is a vampire (release 1)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,9 +5,27 @@ def vampire_check
   age = gets.chomp
   puts "What year were you born in?"
   bday= gets.chomp 
-  puts name
-  puts age 
-  puts bday
+  age_check = 2016 - (bday.to_i)
+  if age.to_i == (2016 - (bday.to_i))
+   puts "Our cafeteria serves garlic bread. Would you like some?"
+   bread = gets.chomp
+     if bread == "no"
+       puts "Would you like to enroll in the company’s health insurance?"
+       health = gets.chomp
+         if health == "yes"
+           puts "great see you at work tommorow Mr. not a vampire"
+         else 
+           puts "AHA YOURE A VAMPIRE!"	
+         end  
+     else 
+       puts "AHA YOURE A VAMPIRE!"
+     end   
+  else
+   puts "AHA YOURE A VAMPIRE!"
+  end  
+
+
+ 
 
 end
 
