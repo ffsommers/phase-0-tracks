355e0df156eddb79b168c26a7504923154a79378
Frank Sommers <franksommers@Franks-MBP.hsd1.ca.comcast.net>
2016-08-05 14:55:41 -0700

    got release 2 and release 3 working.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,8 @@
 def vampire_check 
+  puts "how many employees will be processed"
+  count = gets.chomp.to_i
+idx =0
+  while idx < count
   puts "What is your name?"
   name = gets.chomp
   puts "How old are you?"
@@ -7,46 +11,80 @@ def vampire_check
   bday= gets.chomp 
   puts "Our cafeteria serves garlic bread. Would you like some?"
   bread_user = gets.chomp
+  puts "Would you like to enroll in the company’s health insurance?"
+  health_user = gets.chomp
   
   age_check =  if age.to_i == (2016 - (bday.to_i))
                  age_check =true
                else 
                  age_check = false
                end
+  
   bread =  if bread_user == "yes"
-           bread = false
-           else bread = true
+             bread = false #a vampire would say no to bread
+           else 
+             bread = true
            end  
-  health = puts "Would you like to enroll in the company’s health insurance?"
-           health_user = gets.chomp
-           if health_user == "yes"
-            health = false
+
+  health = if health_user == "yes"
+            health = false # a vampire wouldnt want health insurance
            else 
             health = true
           end
 
  
-  if  age_check && bread && !health  
-        puts "Probably not a vampire."
+ not_vampire = if  age_check &&  !(bread || health)  
+                not_vampire = true
+               else 
+                not_vampire = false
+               end 
    
-  end
-  if !age_check && bread && health  
-        puts "Probably a vampire."
   
-  end      
-  if !age_check && !bread && health
-        puts "Almost certainly a vampire."     
-  end
-  if age_check && bread && !health && name == "Drake Cula" || name == "Tu Fang"
-        puts "Definitely a vampire."
+ prob_vampire = if !(age_check && bread) || !(age_check && health)  
+                  prob_vampire = true 
+                else
+                  prob_vampire = false
+                end    
+  
+        
+  cert_vampire = if !age_check && bread && health
+                 cert_vampire = true
+               else
+                 cert_vampire = false 
+               end
 
-  else
-    puts "Results inconclusive."      
-    
-  end
+  def_vampire = if name == "Drake Cula" || name == "Tu Fang"
+                def_vampire = true
+              else 
+                def_vampire = false      
+              end
+
+     
+     
+  not_sure = if (not_vampire && prob_vampire && cert_vampire && def_vampire)
+              not_sure = true
+             else
+              not_sure = false      
+             end
+
+    if def_vampire 
+       puts "Definitely a vampire." 
+
+     
+    elsif cert_vampire 
+       puts "Almost certainly a vampire."
 
 
- 
+    elsif  prob_vampire && !not_vampire
+       puts "Probably a vampire."
+    elsif  not_vampire
+       puts "Probably not a vampire."
+    else
+       puts "results inconclusive"      
+            
+    end        
+   idx +=1
+ end
 
 end
 
