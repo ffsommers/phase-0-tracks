bd9eeaef31275381fc568885b8177fa07143738a
Frank Sommers <franksommers@Franks-MBP.hsd1.ca.comcast.net>
2016-08-23 12:51:51 -0700

    ran 50 instances of class DRY

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,28 +1,37 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
-
+# I worked on this challenge by myself.
+# Ispent [#] (started at ~ 11:30) hours on this challenge.
+#
+#RELEASE 2
 # EXPLANATION OF require_relative
+# require relative provides method access to the data in state_data.rb file .
+#require_relative complements the builtin method require by allowing you to load a file that is relative to the file containing the require_relative statement.
+#When you use require to load a file, you are usually accessing functionality that has been properly installed, and made accessible, in your system. require does not offer a good solution for loading files within the project’s code.
 #
-#
+#RELEASE 3
+#the state_data file is a datastructure using nested hashes with two different syntaxes. the first level is string and rocket notation for storing the name of the state and a nested hash as the value for providing population density and population size using symbol notation.
+
+
+#For example, if you have unit test classes in the "test" directory, and data for them under the test "test/data" directory, then you might use a line like this in a test case:
+
 require_relative 'state_data'
 
 class VirusPredictor
-
+   # when creating new instance of virusPreductor class the initialize method loads the new instance with some preset data (state of origin , pop density, and population size )
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+   # virus_effects method runs two seperate methods (predicted_deaths and speed_of_spread) and provides provides them the paramaters established in the initialize method which are originally pointing to the state_data file. 
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private
-
+     # predicted_deaths methods calculates the number of people who will die in a state based on conditionals evaluating the population desnsity. The calculation itself relies on multiplying the population size by a given percent based on how the population density conditional is evaluated. the Math.floor built-in method rounds down the value calculated.    
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -40,7 +49,7 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
+   # speed_of_spread method calculates the speed at which the virus will spread based on the population desnity of a state. Conditional blocks evaluate what factor the virus will spread at based on the population density of a state.  
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -68,8 +77,13 @@ end
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
-
+  
+   STATE_DATA.each do |k,v| 
+     k = VirusPredictor.new(k, v[:population_density], v[:population])
+     k.virus_effects
+   end
+  
+=begin
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
@@ -82,6 +96,7 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
+=end
 
 #=======================================================================
 # Reflection Section
