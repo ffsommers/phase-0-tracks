c7093fe7fa9b6a4a2d85dc6cb77ca18181c6ab7c
Frank Sommers <franksommers@Franks-MBP.hsd1.ca.comcast.net>
2016-08-23 13:41:02 -0700

    completed gps

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -26,13 +26,15 @@ class VirusPredictor
   end
    # virus_effects method runs two seperate methods (predicted_deaths and speed_of_spread) and provides provides them the paramaters established in the initialize method which are originally pointing to the state_data file. 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private
+  private # PRIVATE makes restricts access to methods defined within it. Private makes the "new" method (constructor) inaccessible, to force access to an object through some getter function. A typical Singleton implementation is an obvious example.
+
      # predicted_deaths methods calculates the number of people who will die in a state based on conditionals evaluating the population desnsity. The calculation itself relies on multiplying the population size by a given percent based on how the population density conditional is evaluated. the Math.floor built-in method rounds down the value calculated.    
-  def predicted_deaths(population_density, population, state)
+
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -50,7 +52,7 @@ class VirusPredictor
 
   end
    # speed_of_spread method calculates the speed at which the virus will spread based on the population desnity of a state. Conditional blocks evaluate what factor the virus will spread at based on the population density of a state.  
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -100,3 +102,18 @@ alaska.virus_effects
 
 #=======================================================================
 # Reflection Section
+=begin
+  1. What are the differences between the two different hash syntaxes shown in the state_data file?
+     - the first syntax is string / rocket notation, and the nested hash uses symbol notation. 
+
+  2. What does require_relative do? How is it different from require?
+      - require_relative links the te state_data.rb file to the my_solution.rb file. require_relative points to another file to load into your code while require loads files through the interpreter.
+
+  3. What are some ways to iterate through a hash?
+     - .times, .each, sort.map, while, until, for in loops. 
+
+  4. When refactoring virus_effects, what stood out to you about the variables, if anything?
+     - they werent necessary since they were already made global in initialize. 
+
+  5. What concept did you most solidify in this challenge?
+     - variable scope, and the use of private. 
