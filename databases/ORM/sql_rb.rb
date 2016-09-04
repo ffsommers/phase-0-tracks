ec5b217caac4303c9b29b126cfcf279e0b449c7a
Frank Sommers <ssommers21@gmail.com>
2016-09-03 20:09:43 -0700

    created sql database

---
+++ databases/ORM/sql_rb.rb
@@ -0,0 +1,33 @@
+
+
+#create sqlite3 db 
+# add automate fake data using faker
+# user interface
+# retrieve data
+# send data to phone using gem (sms-easy?)
+
+
+# design -- encouraging tweets from fake friends / texts 
+ # create tweets from random people 
+ 	# faker creates the user names
+ 	# create  large arrays for nouns, verbs, adjectives etc to randomly be put together in a tweet.
+
+# require gems
+ require 'sqlite3'
+ require 'faker'
+
+ # create sqlite3 database
+  db = SQLite3::Database.new( "messages.db")
+    
+
+ # learn 
+
+ # string delimiters
+
+ # create a messages table (if its not there already) 
+
+ # add a test message
+
+ # retrieve data
+
+ # add lots of messages (automate)
