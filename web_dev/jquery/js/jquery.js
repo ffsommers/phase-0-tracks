b2adb7f307932ed571e3a1ea89244590508283f2
Frank Sommers <ssommers21@gmail.com>
2016-09-07 14:03:21 -0700

    added slide down event for paragraph

--- web_dev/jquery/js/jquery.js
+++ web_dev/jquery/js/jquery.js
@@ -3,11 +3,12 @@ $(document).ready($(function() {
     $(".logo").ready(function(){
         $(".logo").animate({
             left: '100px',
-            fontSize: '40px'
-            
-            
-
+            fontSize: '40px'     
         });
+    });
+
+    $("h2").click(function(){
+        $(".detailed-info").slideDown("slow");
     });
 
    
