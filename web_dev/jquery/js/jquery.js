ec4b608e61df81ced014d8b7084015171ca5056e
Frank Sommers <ssommers21@gmail.com>
2016-09-07 13:58:17 -0700

    fixed font size change

---
+++ web_dev/jquery/js/jquery.js
@@ -0,0 +1,14 @@
+$(document).ready($(function() {
+
+    $(".logo").ready(function(){
+        $(".logo").animate({
+            left: '100px',
+            fontSize: '40px'
+            
+            
+
+        });
+    });
+
+   
+}));
